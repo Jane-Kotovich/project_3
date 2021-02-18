f77d97e5e7a75168659ff62db7686769da775fbb
Jane Kotovich <jenyakotovich@gmail.com>
2021-02-18 17:27:22 +1000

    decoration

--- index.js
+++ index.js
@@ -7,24 +7,30 @@ const crypto = require("crypto");
 const morgan = require("morgan");
 const { body, validationResult } = require("express-validator");
 const database = require("./database");
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
 
+app.use("/static", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/full-width");
 
 app.listen(PORT, () => {
   console.log("server listening on port 3000");
 });
 app.get("/", (req, res) => {
-  res.render("pages/index");
+  res.render("pages/index", { title: "Home Page" });
 });
 
 app.get("/users", (req, res) => {
   const usersList = data.users;
   res.render("pages/users", {
     usersList: usersList,
+    title: "Users list",
   });
 });
 
@@ -34,6 +40,7 @@ app.get("/users/:userid(\\d+)/", (req, res) => {
   res.render("pages/userid", {
     userId: userId,
     userIdInfo: userIdInfo,
+    title: "Exact user",
   });
 });
 
@@ -53,11 +60,12 @@ app.get("/users/:userid/schedules", (req, res) => {
     results: results,
     usersList: usersList,
     userId: userId,
+    title: "Schedule for choosen user",
   });
 });
 
 app.get("/users/new", (req, res) => {
-  res.render("pages/usersnew");
+  res.render("pages/usersnew", { title: "New user" });
 });
 
 app.post("/users", (req, res) => {
@@ -79,6 +87,7 @@ app.get("/schedules", (req, res) => {
     .then((usersSchedule) => {
       res.render("pages/schedules", {
         schedule: usersSchedule,
+        title: "List of all schedules",
       });
     })
     .catch((err) => {
@@ -93,6 +102,7 @@ app.get("/schedules/new", (req, res) => {
   const usersList = data.users;
   res.render("pages/schnew", {
     usersList: usersList,
+    title: "Add new schedule",
   });
 });
 
